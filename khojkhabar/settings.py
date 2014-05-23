3657d1dd1002a3de422c9791f87e59d9828268a5
bijay <bant4u>
2014-05-23 10:09:53 +0545

    Static file fore gunicorn

--- khojkhabar/settings.py
+++ khojkhabar/settings.py
@@ -134,10 +134,9 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 BASE_STATIC_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = '/static'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    'static',
 )
